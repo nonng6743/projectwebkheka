07ae6220e4b3aceadb634fcfadce63178920cd13
nonng6743 <nonthanakit6743@gmail.com>
2021-06-23 20:03:11 +0700

    first commit

--- components/slick.js
+++ components/slick.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Grid, Paper } from "@material-ui/core";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -7,6 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 export default function App() {
+
   const renderSlides = () =>
     [1, 2, 3, 4, 5, 6, 7, 8].map(num => (
       <div>
@@ -16,7 +17,19 @@ export default function App() {
 
   return (
     <div className="Slick">
+        <Paper elevation={20} >
+      <Grid align="center">
+          <br/>
+        <h1 style={{color:"#000"}}>ประกาศโปรโมชัน สินค้าต่างๆ</h1>
+        <br/>
+      </Grid>
+      <Grid>
       <Slider dots={true}>{renderSlides()}</Slider>
+      <br/>
+      <br/>
+      </Grid>
+      </Paper>
+      
     </div>
   );
 }
