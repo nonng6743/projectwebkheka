07c0d241b1338c4c65eacae5162b88e90ed4c2f8
nonng6743 <nonthanakit6743@gmail.com>
2021-06-23 19:47:23 +0700

    first commit

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Paper } from "@material-ui/core";
 import Layout from '../../components/Layout';
 import Productall from '../../components/products/productall'
+import Slick from '../../components/slick'
 
 const Home = () => {
   const paperStyle = { padding: "30px 20px", width:"90%",  height: "40vh", margin: "20px auto"};
@@ -9,7 +10,7 @@ const Home = () => {
   return (
     <Layout>
     <Grid>
-      
+      <Slick />
       <Paper elevation={20} style={paperStyle}>
       <Grid align="center">
         <h1>ประกาศโปรโมชัน สินค้าต่างๆ</h1>
